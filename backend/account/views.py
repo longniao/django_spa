a289d9d263af210d5a522fa13ef5730cff8c55f3
longniao <longniao@gmail.com>
2022-07-16 12:49:33 -0500

    cache

--- backend/account/views.py
+++ backend/account/views.py
@@ -3,6 +3,9 @@ from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth.hashers import check_password
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
 
 from .models import User
 from .serializers import UserSerializer, TokenSerializer
@@ -22,6 +25,10 @@ class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    @method_decorator(vary_on_cookie)
+    @method_decorator(cache_page(60 * 60))
+    def dispatch(self, *args, **kwargs):
+        return super(UserDetail, self).dispatch(*args, **kwargs)
 
 class UserUpdate(generics.RetrieveUpdateAPIView):
     queryset = User.objects.all()
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -144,3 +144,14 @@ SIMPLE_JWT = {
 AUTHENTICATION_BACKENDS = (
     'account.views.UserAuthorizeWithEmailAndPassword',
 )
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+        "KEY_PREFIX": "assignment",
+    }
+}
